d99e4b6272d427a11773886f06e9199b884212c4
Mohammadhabeeb2001 <71865007+Mohammadhabeeb2001@users.noreply.github.com>
2021-08-11 21:47:20 +0530

    Coding and Data

---
+++ example (2).py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# Julián Brezina
+
+# 100% Human
+
+# In[ ]:
+
+
+#I am interested in data science because I love to gain insights from data.
+
+
+# ### Simple code example
+
+# In[9]:
+
+
+a = (12, 33)
+
+
+# In[10]:
+
+
+b = (33, 12)
+
+
+# In[11]:
+
+
+print (a + b)
+
+
+# In[ ]:
+
+
+
+
